55c7f8c2f8be533eb91b67725b87a823c3f5a3a4
Lucas Santana <santanafx@hotmail.com>
2023-08-29 13:14:34 -0300

    initial commit

---
+++ src/Caracteristicas.java
@@ -0,0 +1,39 @@
+public class Caracteristicas {
+  public double altura;
+  public int peso;
+  public int idade;
+
+  public String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public double getAltura() {
+    return altura;
+  }
+
+  public void setAltura(double altura) {
+    this.altura = altura;
+  }
+
+  public int getPeso() {
+    return peso;
+  }
+
+  public void setPeso(int peso) {
+    this.peso = peso;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+}
---
+++ src/Classe1.java
@@ -0,0 +1,18 @@
+public class Classe1 {
+
+  public static void main(String args[]){
+      Caracteristicas caracteristicas = new Caracteristicas();
+
+      caracteristicas.setNome("Lucas");
+      caracteristicas.setAltura(1.75);
+      caracteristicas.setPeso(75);
+      caracteristicas.setIdade(34);
+
+      System.out.println(caracteristicas.getNome());
+      System.out.println(caracteristicas.getAltura());
+      System.out.println(caracteristicas.getPeso());
+      System.out.println(caracteristicas.getIdade());
+
+
+  }
+}
